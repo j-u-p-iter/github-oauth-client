e2149d66ae27c4093949c3c8d5b4ca4dbda17403
j.u.p.iter <pn79122821224@gmail.com>
2019-08-01 21:38:53 +0500

    continue creating oauth client

--- src/GithubOAuthClient.ts
+++ src/GithubOAuthClient.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { makeUrl } from '@j.u.p.iter/node-utils';
 
+
 import {
   ACCESS_TOKEN_URL,
   AUTHORIZE_APP_URL,
@@ -10,6 +11,7 @@ import {
 
 interface Config {
   clientId: string;
+  clientSecret: string;
   redirectUrl: string;
 } 
 
@@ -26,14 +28,20 @@ export class GithubOAuthClient implements OAuthClient {
   }
 
   private async getAccessToken(code: string): Promise<any> {
-    const { data: { access_token: accessToken } } = await axios.post(ACCESS_TOKEN_URL, { code }); 
+    const params = {
+      code,
+      client_id: this.config.clientId,
+      client_secret: this.config.clientSecret,
+    };
+    
+    const { data: { access_token: accessToken } } = await axios.post(ACCESS_TOKEN_URL, params); 
 
     return accessToken; 
   }
 
   private async getUserData(accessToken: string): Promise<any> {
     const { data: { user } } = await axios.get(USER_URL, {
-      headers: { Authorization: `token ${accessToken}` }
+      headers: { Authorization: `token ${accessToken}` },
     });
 
     return user;
@@ -41,6 +49,7 @@ export class GithubOAuthClient implements OAuthClient {
 
   public generateAuthUrl(): string {
     const authUrl = makeUrl({
+      port: null,
       protocol: 'https',
       host: AUTHORIZE_APP_URL,
       queryObject: {
@@ -48,7 +57,6 @@ export class GithubOAuthClient implements OAuthClient {
         redirect_uri: this.config.redirectUrl,
         scope: SCOPES.join(' '),
       },
-      port: null,
     })
 
     return authUrl;
@@ -56,7 +64,6 @@ export class GithubOAuthClient implements OAuthClient {
 
   public async signIn(code: string): Promise<any> {
     const accessToken = await this.getAccessToken(code);
-
     const userData = await this.getUserData(accessToken);
 
     return this.prepareData(userData);
